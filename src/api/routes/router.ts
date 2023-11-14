92e1d67a96bbc880454e3c048087fc978d59c426
RoyaL <orobertofc@gmail.com>
2023-11-14 13:33:05 -0300

    better routing

--- src/api/routes/router.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { authorize_router } from "./authorize/router.js";
-
-export const routes_router: Router = Router();
-
-routes_router.use("/authorize", authorize_router);
