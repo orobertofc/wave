2b7829bbf94f7ce9458bc57644d7cf7707c5cf80
RoyaL <orobertofc@gmail.com>
2023-11-21 12:10:59 -0300

    changes in ts interfaces

--- src/backend/interfaces/common/Token_interface.ts
+++ src/backend/interfaces/common/Token_interface.ts
@@ -1,5 +1,5 @@
 export interface Token_interface {
-  expires_in: number;
+  expires_in?: number;
   access_token: string;
-  refresh_token: string;
+  refresh_token?: string;
 }
--- src/backend/interfaces/services/Streaming_class.ts
+++ src/backend/interfaces/services/Streaming_class.ts
@@ -1,21 +1,24 @@
 import { Token_interface } from "../common/Token_interface.js";
 import { Response } from "express";
 import { Library_interface } from "../common/Library_interface.js";
+import axios from "axios";
 
 export abstract class Istreaming {
   abstract get_authorization_code(response: Response): Promise<void>;
   abstract get_access_token(
     code: string,
-    needle: any,
+    Axios: typeof axios,
   ): Promise<Token_interface>;
-  abstract refresh_access_token(
+
+  // Deezr wants to be different and does not provide an access token
+  abstract refresh_access_token?(
     refresh_token: string,
-    needle: any,
+    Axios: typeof axios,
   ): Promise<Token_interface>;
   abstract get_library(access_token: string): Promise<Library_interface>;
   abstract create_playlist(
     name: string,
     access_token: string,
-    needle: any,
+    Axios: typeof axios,
   ): Promise<any>;
 }
